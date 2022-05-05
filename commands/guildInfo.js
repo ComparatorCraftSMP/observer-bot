6f38be201eb0b027aac7c077af7989d13118c845
gboys2008@gmail.com <gboys2008@gmail.com>
2022-05-05 11:26:01 -0400

    new command and boiler plate for the command

---
+++ commands/guildInfo.js
@@ -0,0 +1,37 @@
+const {SlashCommandBuilder, SlashCommandStringOption} = require('@discordjs/builders');
+const {MessageEmbed, CommandInteractionOptionResolver, Message} = require('discord.js');
+const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
+const dotenv = require('dotenv');
+const { botIcon, botUsername } = require('../events/ready');
+const { embedColor, scoreboard } = require('../config.json')
+dotenv.config();
+
+
+
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('guildinfo')
+        .setDescription('information about this discord guild'),
+    
+    async execute(interaction) {
+
+        
+    try {
+
+        const embed = new MessageEmbed()
+                  .setColor(`${embedColor}`)
+                  .setTitle(`ComparatorCraftSMP's Plugins`)
+                  .setDescription(`${pluginString}`)
+        
+        await interaction.reply({embeds: [embed]}) 
+          
+        
+
+        console.log(`${interaction.user.tag} did /plugins in ${interaction.channel.name} in guild ${interaction.guild.name}`)    
+        } catch(error) {
+            await interaction.reply({ content: 'This server has 0 plugins', ephemeral: true })
+            console.error(error)
+        }
+    }
+}
