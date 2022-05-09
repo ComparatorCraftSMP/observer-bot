4857d195f5dcb4a50f55feac4c17500925f6868a
gboys2008@gmail.com <gboys2008@gmail.com>
2022-05-09 10:45:56 -0400

    boiler plate for the command

---
+++ commands/guildMemberInfo.js
@@ -0,0 +1,53 @@
+const {SlashCommandBuilder, SlashCommandStringOption} = require('@discordjs/builders');
+const {MessageEmbed, CommandInteractionOptionResolver, Message, Client} = require('discord.js');
+const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
+const dotenv = require('dotenv');
+const { botIcon, botUsername } = require('../events/ready');
+const { embedColor, scoreboard } = require('../config.json')
+const client = require('../index.js')
+dotenv.config();
+
+
+
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('guildmemberinfo')
+        .setDescription('information about the selected discord user'),
+    
+    async execute(interaction) {
+
+        
+    try {
+        const gld = interaction.guild
+
+        const cmd = await client.application.commands.fetch()
+
+        const chnl = await gld.channels.fetch()
+        const chnlCount = chnl.size
+
+        const roles = await gld.roles.fetch()
+        const roleCount = roles.size
+
+        const undRemove = /(_)/g
+
+        const feature = gld.features.map(fe => `✅ ${fe.replace(undRemove, ' ').toLowerCase().replace(/(^|\s)\S/g, L => L.toUpperCase())}`).join('\n')
+
+        const embed = new MessageEmbed()
+                  .setColor(`${embedColor}`)
+                  .setTitle(`Information about ${interaction.user.username}`)
+                  .addFields(
+    
+                  )
+                  .setThumbnail(interaction.user.avatarURL({dynamic:true}))
+        
+        await interaction.reply({embeds: [embed]}) 
+        
+
+        console.log(`${interaction.user.tag} did /guildinfo in ${gld.name} in guild ${gld.name}`)    
+        } catch(error) {
+            await interaction.reply({ content: 'Error', ephemeral: true })
+            console.error(error)
+        }
+    }
+}
