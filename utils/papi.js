2313fbf2d074327e388e5045246c2a8b1cfad734
gboys2008@gmail.com <gboys2008@gmail.com>
2022-05-11 11:57:35 -0400

    basic post params

--- utils/papi.js
+++ utils/papi.js
@@ -4,6 +4,12 @@ dotenv.config();
 
 
 const fetchPlaceholder = async (uuid, plh)  => {
+    const options = {
+        method: 'POST',
+        headers: {'Content-Type': 'application/x-www-form-urlencoded', 'key': `${process.env.API}`}
+      }
+
+    const response = await fetch(`${process.env.SERVER}/v1/placeholders/replace`, options)
 
 }
 
