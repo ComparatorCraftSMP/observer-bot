37abcf17ea4f1d25636fa532ca1eabee1993e49d
gboys2008@gmail.com <gboys2008@gmail.com>
2022-05-12 10:33:10 -0400

    converts placeholder and adds query url

--- utils/papi.js
+++ utils/papi.js
@@ -4,13 +4,14 @@ dotenv.config();
 
 
 const fetchPlaceholder = async (uuid, plh)  => {
+    const placeholder = plh.replace("%", "%25")
     const options = {
         method: 'POST',
         headers: {'Content-Type': 'application/x-www-form-urlencoded', 'key': `${process.env.API}`}
       }
 
-    const response = await fetch(`${process.env.SERVER}/v1/placeholders/replace`, options)
-
+    const response = await fetch(`${process.env.SERVER}/v1/placeholders/replace?message=${placeholder}25&uuid=${uuid}`, options)
+    return response
 }
 
 module.exports = fetchPlaceholder
