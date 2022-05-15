de63fedd95942795a1514f6370e2a82b9e295b63
BossDaily <comparatorcraft2020@gmail.com>
2022-05-15 09:40:24 -0500

    just a normal return statement

--- utils/papi.js
+++ utils/papi.js
@@ -9,16 +9,14 @@ const fetchPlaceholder = async (uuid, plh)  => {
     const options = {
         method: 'POST',
         headers: {'accept': 'application/json', 'Content-Type': 'application/x-www-form-urlencoded', 'key': `${process.env.API}`},
-        body: `message=${placeholder}25&uuid=${uuid}`
+        body: `message=${placeholder}&uuid=${uuid}`
         //body: 'message=%25discordsrv_user_name%25&uuid=8b005697-0c91-42bd-b404-9e065e08fbb8'
       }
 
     const response = await fetch(`${process.env.SERVER}/v1/placeholders/replace`, options)
     
     const data = await response.text()
-      .then( data =>{
-        console.log(data)
-      })
+    return data.replace(/(")/g, '')
     
 }
 
