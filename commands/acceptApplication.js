f27e1f8c0544451e5eb0aaa9553056ec2450826c
gboys2008@gmail.com <gboys2008@gmail.com>
2022-07-21 15:04:26 -0400

    stuff

--- commands/acceptApplication.js
+++ commands/acceptApplication.js
@@ -1,39 +1,66 @@
-const {SlashCommandBuilder, SlashCommandStringOption, ContextMenuCommandBuilder} = require('@discordjs/builders');
-const {MessageEmbed, CommandInteractionOptionResolver, Message, Client, ClientUser, Guild } = require('discord.js');
-const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
-const dotenv = require('dotenv');
-const { botIcon, botUsername } = require('../events/ready');
-const { embedColor, scoreboard, application } = require('../config.json')
+const {
+  SlashCommandBuilder,
+  SlashCommandStringOption,
+  ContextMenuCommandBuilder,
+} = require("@discordjs/builders");
+const {
+  MessageEmbed,
+  CommandInteractionOptionResolver,
+  Message,
+  Client,
+  ClientUser,
+  Guild,
+} = require("discord.js");
+const fetch = (...args) =>
+  import("node-fetch").then(({ default: fetch }) => fetch(...args));
+const dotenv = require("dotenv");
+const { botIcon, botUsername } = require("../events/ready");
+const { embedColor, scoreboard, application } = require("../config.json");
 
 dotenv.config();
 
-
 module.exports = {
-    data: new ContextMenuCommandBuilder()
-        .setName('Accept Application')
-        .setType(3),
-        
-    
-    async execute(interaction) {  
-        const msg = interaction.targetMessage
-        const cmdUser = interaction.member
-        const applicant = interaction.targetMessage.member
-        try {
-            if(msg.channel.parentId === application.ticket_category) {
-                if (cmdUser.roles.cache.find(role => role.id === application.staff_role) || cmdUser.permissions.has('MANAGE_ROLES')) {
-                    await msg.reply({ content:`${application.message}`})
-                    await interaction.reply({ content:'Message sent', ephemeral: true})
-                    await applicant.roles.add(application.member_role)
-                    await applicant.roles.remove(application.applicant_role)
-                } else {
-                    await interaction.reply({ content:'You dont have permission to send this command', ephemeral: true})
-                }
-            } else {
-                await interaction.reply({ content:"This isn't an application", ephemeral: true})
-            }
-            
-        } catch(error) {
-            console.error(error)
+  data: new ContextMenuCommandBuilder()
+    .setName("Accept Application")
+    .setType(3),
+
+  async execute(interaction) {
+    const msg = interaction.targetMessage;
+    const cmdUser = interaction.member;
+    const applicant = interaction.targetMessage.member;
+    try {
+      if (msg.channel.parentId === application.ticket_category) {
+        if (
+          cmdUser.roles.cache.find(
+            (role) => role.id === application.staff_role
+          ) ||
+          cmdUser.permissions.has("MANAGE_ROLES")
+        ) {
+          if (applicant.user.bot) {
+            await interaction.reply({
+              content:
+                "This is a bot, please run this command on the application not a bot message",
+              ephemeral: true,
+            });
+          }
+          applicant.roles.add(application.member_role);
+          applicant.roles.remove(application.applicant_role);
+          await msg.reply({ content: `${application.message}` });
+          await interaction.reply({ content: "Message sent", ephemeral: true });
+        } else {
+          await interaction.reply({
+            content: "You dont have permission to send this command",
+            ephemeral: true,
+          });
         }
+      } else {
+        await interaction.reply({
+          content: "This isn't an application",
+          ephemeral: true,
+        });
+      }
+    } catch (error) {
+      console.error(error);
     }
-}
+  },
+};
