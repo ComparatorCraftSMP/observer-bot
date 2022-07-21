900c2da3c68ceab82b7f20922783ba87a8518f40
gboys2008@gmail.com <gboys2008@gmail.com>
2022-07-21 15:04:39 -0400

    guild members intent

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const client = new Client({ intents: [
             Intents.FLAGS.GUILD_MESSAGES,
             Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
             Intents.FLAGS.GUILD_MESSAGE_TYPING,
+            Intents.FLAGS.GUILD_MEMBERS,
             Intents.FLAGS.DIRECT_MESSAGES,
             Intents.FLAGS.DIRECT_MESSAGE_REACTIONS,
             Intents.FLAGS.DIRECT_MESSAGE_TYPING,
